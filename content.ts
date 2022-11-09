464afc7138578268fe7b1bc5821267012fd0ab9d
aaaaninja <aaaaninja@aaaa.ninja>
2022-11-10 06:06:14 +0900

    deno lint (no-window-prefix) and fmt

--- content.ts
+++ content.ts
@@ -1,36 +1,37 @@
-import { finder } from "@medv/finder"
-import querySelectorAllWithHas from "~core/polyfill-css-has"
-import type { PlasmoContentScript } from "plasmo"
+import { finder } from '@medv/finder';
+import querySelectorAllWithHas from '~core/polyfill-css-has';
+import type { PlasmoContentScript } from 'plasmo';
 
 export const config: PlasmoContentScript = {
-  matches: ["https://*.twitter.com/*"],
-  all_frames: true
-}
+  matches: ['https://*.twitter.com/*'],
+  all_frames: true,
+};
 
-window.addEventListener("load", () => {
-    let isPressingQ = false // 任意のキーコンビネーションをやるため。(shiftやctrlとかだったらEventに含まれてるんだけどね......)
-    document.addEventListener("keydown", e => isPressingQ = (e.key === "q"))
-    document.addEventListener("keyup", () => isPressingQ = false)
+addEventListener('load', () => {
+  let isPressingQ = false; // 任意のキーコンビネーションをやるため。(shiftやctrlとかだったらEventに含まれてるんだけどね......)
+  document.addEventListener('keydown', (e) => isPressingQ = e.key === 'q');
+  document.addEventListener('keyup', () => isPressingQ = false);
 
-    document.addEventListener('click', (e) => {
-        if (!(e.target instanceof HTMLElement)) { console.log('not HTMLELEMENT!', e); return }
+  document.addEventListener('click', (e) => {
+    if (!(e.target instanceof HTMLElement)) {
+      console.log('not HTMLELEMENT!', e);
+      return;
+    }
 
-        if (!isPressingQ || e.target.tagName !== "IMG") { return }
-        e.stopPropagation()
-        e.preventDefault()
+    if (!isPressingQ || e.target.tagName !== 'IMG') return;
+    e.stopPropagation();
+    e.preventDefault();
 
-        const clickedElSelector = finder(e.target)
-        const tweetSelector = `article:has(${clickedElSelector})`
-        const tweet = querySelectorAllWithHas(tweetSelector)
+    const clickedElSelector = finder(e.target);
+    const tweetSelector = `article:has(${clickedElSelector})`;
+    const tweet = querySelectorAllWithHas(tweetSelector);
 
-        console.log(isPressingQ)
-        console.log(e)
-        console.log(e.target)
-        console.log(tweetSelector)
-        console.log(tweet)
+    console.log(isPressingQ);
+    console.log(e);
+    console.log(e.target);
+    console.log(tweetSelector);
+    console.log(tweet);
+  }, true);
 
-    }, true)
-
-
-  console.log("content script loaded")
-})
+  console.log('content script loaded');
+});
